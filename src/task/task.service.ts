4e6db5f6040e416c2aae7983f72ff72d68bb182a
dev-davimarques <davimarques10.dm@icloud.com>
2025-04-13 01:08:03 -0300

    novas rotas - find by id e update

--- src/task/task.service.ts
+++ src/task/task.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { TaskDto } from './task.dto';
 
 @Injectable()
@@ -9,5 +9,28 @@ export class TaskService {
   create(task: TaskDto) {
     this.tasks.push(task);
     console.log(this.tasks);
+  }
+
+  findById(id: string): TaskDto {
+    const foundTask = this.tasks.filter((t) => t.id === id);
+    if (foundTask.length) {
+      return foundTask[0];
+    }
+    throw new HttpException(
+      `Task with id ${id} not found`,
+      HttpStatus.NOT_FOUND,
+    );
+  }
+
+  update(task: TaskDto) {
+    const taskIndex = this.tasks.findIndex((t) => t.id === task.id);
+    if (taskIndex >= 0) {
+      this.tasks[taskIndex] = task;
+      return;
+    }
+    throw new HttpException(
+      `Task with id ${task.id} not found`,
+      HttpStatus.BAD_REQUEST,
+    );
   }
 }
