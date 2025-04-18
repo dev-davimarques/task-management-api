fd87ce5fcf53f275bb27c3655e51e11448fd3d82
dev-davimarques <davimarques10.dm@icloud.com>
2025-04-18 13:34:53 -0300

    modulo users

---
+++ src/users/users.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@nestjs/common';
+import { UserDto } from './user.dto';
+import { v4 as uuid } from 'uuid';
+import { hashSync as bcryptHashSync } from 'bcrypt';
+
+@Injectable()
+export class UsersService {
+  // Dados mockcados
+  private readonly users: UserDto[] = [];
+
+  create(newUser: UserDto) {
+    newUser.id = uuid();
+    newUser.password = bcryptHashSync(newUser.password, 10);
+    this.users.push(newUser);
+    // console.log(this.users);
+  }
+}
